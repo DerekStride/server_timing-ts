ac922779898864b484cc2901894091cbd43e084a
derekstride <derek.stride@shopify.com>
2025-01-10 14:43:54 -0500

    feat: extract to query tree and build ruby data object

--- lib/server_timing.rb
+++ lib/server_timing.rb
@@ -9,6 +9,12 @@ module ServerTiming
   class ParserMissing < Error; end
   class ServerTimingHeaderMissing < Error; end
 
+  TimingMetric = Data.define(:name, :duration, :description) do
+    def to_metric = "#{name};dur=#{dur};desc=#{desc}"
+    def dur = duration
+    def desc = description
+  end
+
   def parser
     @parser ||= begin
       TreeStand.configure do
@@ -25,5 +31,19 @@ module ServerTiming
     header = Net::HTTP.get_response(URI(url)).[]("Server-Timing")
     raise ServerTimingHeaderMissing if header.nil?
     header
+  end
+
+  def extract(tree)
+    tree.query(<<~QUERY).map do |match|
+      (timing_metric
+        name: (token) @name
+        duration: (duration value: (number) @duration)?
+        description: (description value: (string) @description)?)
+    QUERY
+      name = match.fetch("name").text
+      duration = match["duration"]&.text&.to_f
+      description = match["description"]&.text
+      TimingMetric.new(name:, duration:, description:)
+    end
   end
 end
